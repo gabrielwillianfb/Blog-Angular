993de983b197cd4d57b412e4f25d0fc3860d348b
gabrielwillianfb <gabriel.beck03@gmail.com>
2023-07-25 20:59:34 -0300

    Projeto no-responsive Blog Angular, concluído.

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Uma bela paisagem com um rio em um planalto",
+        "image": "../../../assets/paisagem.jpg",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora provident sed aspernatur iusto voluptatum distinctio reprehenderit dolorem libero? Deleniti necessitatibus maxime sequi vero corporis quas quidem beatae. Autem, fuga esse!"
+    },
+    {
+        "id": "2",
+        "title": "Prós e contras ao praticar ciclismo no dia a dia",
+        "image": "../../../assets/ciclista.jpg",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde culpa est magni vitae rem quasi aliquam facilis fugiat eum odit, quae enim dolore ipsa voluptas beatae dignissimos? Ex, possimus delectus!"
+    },
+    {
+        "id": "3",
+        "title": "Os impactos da Inteligência Artificial - IA em nossas vidas",
+        "image": "../../../assets/Inteligencia-artificial.jpg",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quis, magni nesciunt optio placeat et dolor nisi aperiam, tenetur expedita eligendi possimus laborum. A non neque corporis, laboriosam nostrum animi?"
+    },
+    {
+        "id": "4",
+        "title": "Qual foi a importância das Criptomoedas no sistema econômico",
+        "image": "../../../assets/cripto.jpg",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat minus quis incidunt veniam doloribus molestiae accusantium sapiente nam accusamus exercitationem quo tenetur ratione nesciunt id, iure optio nostrum, eveniet repudiandae!"
+    }
+]
